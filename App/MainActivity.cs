0cc8b355f30ed89910d5e0076121fbb8dec006e9
Jan Rabe <jan.rabe@exozet.com>
2018-07-18 15:09:37 +0200

    added demo app and fixed imports for generating nuget

---
+++ App/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+using Com.Rbrooks.Indefinitepagerindicator;
+using Android.Support.V7.App;
+
+namespace App
+{
+    [Activity(Label = "App", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var indicicator = FindViewById<IndefinitePagerIndicator>(Resource.Id.recyclerview_pager_indicator);
+        }
+    }
+}
